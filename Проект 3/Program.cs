da92aae4d9e47d204e0fed600ec52e7acbd0020d
ya.zmeya.2001@gmail.com <ya.zmeya.2001@gmail.com>
2019-10-08 21:50:04 +0300

    Добавьте файлы проекта.

---
+++ Проект 3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Проект_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Определение угла");
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Minutes: ");
+                int min = int.Parse(Console.ReadLine());
+                Console.Write("Seconds: ");
+                int sec = int.Parse(Console.ReadLine());
+                double angle = hours * 30 + min / 2 + sec / 120;
+                Console.WriteLine(angle);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
